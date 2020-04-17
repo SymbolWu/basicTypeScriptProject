61bdfa2dffd96ef4c5e2cf225474847f485c3ed7
wuxinbo <wuxinbo@wuxinbodeMacBook-Pro.local>
2020-04-17 23:10:22 +0800

    css module中更改Antd的样式

--- src/TestDemo/index.tsx
+++ src/TestDemo/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Button, Collapse } from 'antd';
 import styles from './style.less';
 
+const { Panel } = Collapse;
 interface TestProps {
   title: string
+}
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+function callback(key: any) {
+  console.log(key);
 }
 
 const Test: React.FC<TestProps> = (props) => {
@@ -14,6 +26,24 @@ const Test: React.FC<TestProps> = (props) => {
   return (
     <div className={styles.title}>
       {getTitle(title)}
+      <Button type="primary">Test</Button>
+      <div className={styles.collapse}>
+        <Collapse
+          defaultActiveKey={['1']}
+          onChange={callback}
+          className={styles.container}
+        >
+          <Panel header="This is panel header 1" key="1">
+            <p>{text}</p>
+          </Panel>
+          <Panel header="This is panel header 2" key="2">
+            <p>{text}</p>
+          </Panel>
+          <Panel header="This is panel header 3" key="3" disabled>
+            <p>{text}</p>
+          </Panel>
+        </Collapse>
+      </div>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/TestDemo';
+import 'antd/dist/antd.css';
 ReactDOM.render(<App title="Home"/>, document.getElementById('root'));
